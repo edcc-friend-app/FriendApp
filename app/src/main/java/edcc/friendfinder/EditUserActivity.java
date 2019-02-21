4f2d08fe807a1f9b8d58e37b4047ebdd6cadeb62
Estefano Felipa <estefano_fc@hotmail.com>
2019-02-21 15:42:48 -0800

    Added fields for the editFriend class E.F.C

--- app/src/main/java/edcc/friendfinder/EditUserActivity.java
+++ app/src/main/java/edcc/friendfinder/EditUserActivity.java
@@ -1,23 +1,39 @@
 package edcc.friendfinder;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
 
 public class EditUserActivity extends AppCompatActivity {
+
+    private Bitmap photo;
+    private EditText firstName;
+    private EditText lastName;
+    private EditText major;
+    private EditText age;
+    private EditText firstclasses;
+    private EditText bio;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_user);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
+    public void ibtnUserPhotoOnClick(View view) {
+
+
+    }
 }
