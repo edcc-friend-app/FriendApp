8d88ab97464fbb23648c089e24d855795ec297c8
jonathanyoung <jonathan.p.young@gmail.com>
2019-02-21 15:46:38 -0800

    -Java EditUserActivity- added a commented out ibtnOnClick

--- app/src/main/java/edcc/friendfinder/EditUserActivity.java
+++ app/src/main/java/edcc/friendfinder/EditUserActivity.java
@@ -1,6 +1,8 @@
 package edcc.friendfinder;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -19,5 +21,24 @@ public class EditUserActivity extends AppCompatActivity {
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+
+    /**
+     * Event handler for the photo button click event.
+     *
+     * @param view the photo button
+     */
+//    public void ibtnUserPhotoOnClick(View view) {
+//        dispatchTakePictureIntent();
+//    }
+//
+//    /**
+//     * Sends control to a camera app to get a thumbnail.
+//     */
+//    private void dispatchTakePictureIntent() {
+//        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+//            startActivityForResult(takePictureIntent, Extras.REQUEST_IMAGE_CAPTURE);
+//        }
+//    }
 
 }
