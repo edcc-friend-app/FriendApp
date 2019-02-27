d43ad93cf8447c875eb72cc455d8ff292b3018a1
Estefano Felipa <estefano_fc@hotmail.com>
2019-02-26 16:20:48 -0800

    I did a lot of stuff EFC

--- app/src/main/java/edcc/friendfinder/ProfileFragment.java
+++ app/src/main/java/edcc/friendfinder/ProfileFragment.java
@@ -1,11 +1,22 @@
 package edcc.friendfinder;
 
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 
 /**
@@ -13,6 +24,10 @@ import android.view.ViewGroup;
  */
 public class ProfileFragment extends Fragment {
 
+    private View rootView;
+    private ImageButton ibtnUserPhoto;
+    private TextView lblInfo;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -23,7 +38,34 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        rootView = inflater.inflate(R.layout.fragment_profile, container, false);
+        ibtnUserPhoto = rootView.findViewById(R.id.ibtnUserPhoto);
+        //lblInfo = rootView.findViewById(R.id.lblInfo);
+        return rootView;
     }
+
+
+
+//    public void ibtnUserPhotoOnClick(View view) {
+//        dispatchTakePictureIntent();
+//    }
+
+//    public void dispatchTakePictureIntent(){
+//        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+//            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+//        }
+//    }
+
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+//        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+//            Bundle extras = data.getExtras();
+//            Bitmap imageBitMap = (Bitmap) extras.get("data");
+//            ibtnUserPhoto.setImageBitmap(imageBitMap);
+//            String info = (imageBitMap.getWidth() + "x" + imageBitMap.getHeight());
+//            lblInfo.setText(info);
+//        }
+//    }
 
 }
