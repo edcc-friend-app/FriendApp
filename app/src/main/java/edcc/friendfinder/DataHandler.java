d86289ff064cad307216de5c717849d988c53359
Anthony Luong <a.luong1997@edmail.edcc.edu>
2019-02-27 21:33:36 -0800

    //sample friends, almost fully different ppl

--- app/src/main/java/edcc/friendfinder/DataHandler.java
+++ app/src/main/java/edcc/friendfinder/DataHandler.java
@@ -9,6 +9,16 @@ public class DataHandler {
     Course math = new Course("MATH 151", "Richard D");
     Course cs = new Course("CS 141", "Linda Z");
     Course english = new Course("ENG 101", "Kumar P");
+    User friend1;
+    User friend2;
+    User friend3;
+    User friend4;
+    User friend5;
+    User friend6;
+    User friend7;
+    User friend8;
+    User friend9;
+    User friend10;
 
     //String array of courses
 
@@ -30,13 +40,36 @@ public class DataHandler {
         classes.add(math);
         classes.add(cs);
         classes.add(english);
-
-        list.add(new User("Franshesco", "Coello","CS", "Soccer",
-                friends, classes, 955123123, "English"));
-        list.add(new User("Anthony", "Luong", "CS", "Memes",
-                friends, classes, 955111321, "Vietnamese"));
-        list.add(new User("Jonathan", "Young", "CS", "Football",
-                friends, classes, 955008130, "Chinese"));
+        friend1 = new User("Franshesco", "Coello","CS", "Soccer",
+                friends, classes, 955123123, "English");
+        list.add(friend1);
+        friend2 = new User("Anthony", "Luong", "CS", "Memes",
+                friends, classes, 955111321, "Vietnamese");
+        list.add(friend2);
+        friend3 = new User("Jonathan", "Young", "Mathematics", "Football",
+                friends, classes, 955008130, "Chinese");
+        list.add(friend3);
+        friend4 = new User("Susy", "Lincoln", "English", "Football",
+                friends, classes, 955008130, "Chinese");
+        list.add(friend4);
+        friend5 = new User("Jared", "Kuddes", "English", "Football",
+                friends, classes, 955008130, "Chinese");
+        list.add(friend5);
+        friend6 = new User("Sora", "Fung", "Biology", "Football",
+                friends, classes, 955008130, "Chinese");
+        list.add(friend6);
+        friend7 = new User("Roxas", "Pham", "Mathematics", "Football",
+                friends, classes, 955008130, "Chinese");
+        list.add(friend7);
+        friend8 = new User("Vivian", "Nguyen", "Biology", "Football",
+                friends, classes, 955008130, "Chinese");
+        list.add(friend8);
+        friend9 = new User("Henry", "Zokkins", "Art", "Football",
+                friends, classes, 955008130, "Chinese");
+        list.add(friend9);
+        friend10 = new User("Vy", "Bui", "Art", "Football",
+                friends, classes, 955008130, "Chinese");
+        list.add(friend10);
     }
 
     public ArrayList<User> getUsers() {
